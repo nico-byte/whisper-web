bb99eaa3c22809586c286fc1945cfb47e147e78f
nico-byte <nicofuchs653@outlook.de>
2025-06-18 10:00:27 +0200

    removed volume from setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,3 @@ cp .env.example .env
 echo "Creating .models directory..."
 mkdir -p .models
 chmod 755 .models
-
-echo "Creating Docker volume for models..."
-docker volume create --driver local -o o=bind -o type=none -o device="${PWD}/.models" models
